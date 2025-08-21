6fcbfe9209cccdcae9d9cd88a80f430d71a81942
lucascjesus6-pixel <lucas.cjesus6@aluno.edu.es.gov.br>
2025-08-20 23:29:29 -0300

    Update Card.tsx

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,1 +1,12 @@
+type Props = {
+  title: string
+}
+
+export default function Card({ title }: Props) {
+  return (
+    <div className="bg-white p-6 rounded-xl shadow hover:shadow-lg transition">
+      <h3 className="text-lg font-semibold text-purple-700">{title}</h3>
+    </div>
+  )
+}
 
