4b40ac87cfce072a90509a906ed4348cea8c8256
lucascjesus6-pixel <lucas.cjesus6@aluno.edu.es.gov.br>
2025-08-21 00:27:40 -0300

    Update Card.tsx

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,7 +1,13 @@
+interface Props {
+  title: string;
+}
+
 export default function Card({ title }: Props) {
   return (
-    <div className="bg-white p-6 rounded-2xl shadow-xl hover:shadow-2xl transform hover:scale-105 transition duration-300 cursor-pointer">
-      <h3 className="text-lg font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-600 to-pink-500">{title}</h3>
+    <div className="bg-white p-6 rounded-2xl shadow-xl hover:shadow-2xl transition-shadow duration-300">
+      <h3 className="text-lg font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+        {title}
+      </h3>
     </div>
-  )
+  );
 }
